172396f2c6cd04dfadb7e991e1287cbdf54d7c9e
Muhammad Moaz <158042944+moazsaeedantechpvt@users.noreply.github.com>
2024-09-06 17:26:56 +0500

    card components

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Spinner from "../../components/Spinner";
+import BenefitsCard from "../../components/BenifitsCart";
+import { FaCode, FaComputer } from "react-icons/fa6";
+import ServiceHomeCard from "../../components/ServiceHomeCard";
 
 const Landing = () => {
+  const webDevStacks = ['React.js', 'Node.js', 'Laravel', 'MongoDB', 'Tailwind CSS'];
   return (
     <div>
-      <h1>Hellloooooo</h1>
+      <h1>helooooooooooo</h1>
+     
     </div>
   );
 };
